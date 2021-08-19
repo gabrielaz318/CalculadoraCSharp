3d8bf46c625de4cfc2d30181fa0f2149bb051622
Gabriel Azevedo <gabrielazevedo318@gmail.com>
2021-08-19 20:36:08 -0300

    Primeira versão da calculadora

---
+++ Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿
+namespace How3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.saida = new System.Windows.Forms.TextBox();
+            this.c = new System.Windows.Forms.Button();
+            this.quatro = new System.Windows.Forms.Button();
+            this.cinco = new System.Windows.Forms.Button();
+            this.nove = new System.Windows.Forms.Button();
+            this.oito = new System.Windows.Forms.Button();
+            this.sete = new System.Windows.Forms.Button();
+            this.seis = new System.Windows.Forms.Button();
+            this.um = new System.Windows.Forms.Button();
+            this.tres = new System.Windows.Forms.Button();
+            this.dois = new System.Windows.Forms.Button();
+            this.soma = new System.Windows.Forms.Button();
+            this.zero = new System.Windows.Forms.Button();
+            this.ponto = new System.Windows.Forms.Button();
+            this.subtracao = new System.Windows.Forms.Button();
+            this.divisao = new System.Windows.Forms.Button();
+            this.multiplicacao = new System.Windows.Forms.Button();
+            this.igual = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // saida
+            // 
+            this.saida.BackColor = System.Drawing.SystemColors.Window;
+            this.saida.Cursor = System.Windows.Forms.Cursors.Default;
+            this.saida.Location = new System.Drawing.Point(12, 12);
+            this.saida.Multiline = true;
+            this.saida.Name = "saida";
+            this.saida.ReadOnly = true;
+            this.saida.Size = new System.Drawing.Size(266, 31);
+            this.saida.TabIndex = 0;
+            this.saida.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // c
+            // 
+            this.c.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.c.Location = new System.Drawing.Point(12, 62);
+            this.c.Name = "c";
+            this.c.Size = new System.Drawing.Size(124, 51);
+            this.c.TabIndex = 1;
+            this.c.Text = "C";
+            this.c.UseVisualStyleBackColor = true;
+            this.c.Click += new System.EventHandler(this.c_Click);
+            // 
+            // quatro
+            // 
+            this.quatro.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.quatro.Location = new System.Drawing.Point(12, 199);
+            this.quatro.Name = "quatro";
+            this.quatro.Size = new System.Drawing.Size(53, 51);
+            this.quatro.TabIndex = 2;
+            this.quatro.Text = "4";
+            this.quatro.UseVisualStyleBackColor = true;
+            this.quatro.Click += new System.EventHandler(this.quatro_Click);
+            // 
+            // cinco
+            // 
+            this.cinco.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.cinco.Location = new System.Drawing.Point(83, 200);
+            this.cinco.Name = "cinco";
+            this.cinco.Size = new System.Drawing.Size(53, 51);
+            this.cinco.TabIndex = 3;
+            this.cinco.Text = "5";
+            this.cinco.UseVisualStyleBackColor = true;
+            this.cinco.Click += new System.EventHandler(this.cinco_Click);
+            // 
+            // nove
+            // 
+            this.nove.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.nove.Location = new System.Drawing.Point(154, 131);
+            this.nove.Name = "nove";
+            this.nove.Size = new System.Drawing.Size(53, 51);
+            this.nove.TabIndex = 4;
+            this.nove.Text = "9";
+            this.nove.UseVisualStyleBackColor = true;
+            this.nove.Click += new System.EventHandler(this.nove_Click);
+            // 
+            // oito
+            // 
+            this.oito.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.oito.Location = new System.Drawing.Point(83, 131);
+            this.oito.Name = "oito";
+            this.oito.Size = new System.Drawing.Size(53, 51);
+            this.oito.TabIndex = 5;
+            this.oito.Text = "8";
+            this.oito.UseVisualStyleBackColor = true;
+            this.oito.Click += new System.EventHandler(this.oito_Click);
+            // 
+            // sete
+            // 
+            this.sete.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.sete.Location = new System.Drawing.Point(12, 131);
+            this.sete.Name = "sete";
+            this.sete.Size = new System.Drawing.Size(53, 51);
+            this.sete.TabIndex = 6;
+            this.sete.Text = "7";
+            this.sete.UseVisualStyleBackColor = true;
+            this.sete.Click += new System.EventHandler(this.sete_Click);
+            // 
+            // seis
+            // 
+            this.seis.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.seis.Location = new System.Drawing.Point(154, 200);
+            this.seis.Name = "seis";
+            this.seis.Size = new System.Drawing.Size(53, 51);
+            this.seis.TabIndex = 8;
+            this.seis.Text = "6";
+            this.seis.UseVisualStyleBackColor = true;
+            this.seis.Click += new System.EventHandler(this.seis_Click);
+            // 
+            // um
+            // 
+            this.um.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.um.Location = new System.Drawing.Point(12, 268);
+            this.um.Name = "um";
+            this.um.Size = new System.Drawing.Size(53, 51);
+            this.um.TabIndex = 9;
+            this.um.Text = "1";
+            this.um.UseVisualStyleBackColor = true;
+            this.um.Click += new System.EventHandler(this.um_Click);
+            // 
+            // tres
+            // 
+            this.tres.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.tres.Location = new System.Drawing.Point(154, 269);
+            this.tres.Name = "tres";
+            this.tres.Size = new System.Drawing.Size(53, 51);
+            this.tres.TabIndex = 10;
+            this.tres.Text = "3";
+            this.tres.UseVisualStyleBackColor = true;
+            this.tres.Click += new System.EventHandler(this.tres_Click);
+            // 
+            // dois
+            // 
+            this.dois.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.dois.Location = new System.Drawing.Point(83, 269);
+            this.dois.Name = "dois";
+            this.dois.Size = new System.Drawing.Size(53, 51);
+            this.dois.TabIndex = 11;
+            this.dois.Text = "2";
+            this.dois.UseVisualStyleBackColor = true;
+            this.dois.Click += new System.EventHandler(this.dois_Click);
+            // 
+            // soma
+            // 
+            this.soma.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.soma.Location = new System.Drawing.Point(154, 62);
+            this.soma.Name = "soma";
+            this.soma.Size = new System.Drawing.Size(53, 51);
+            this.soma.TabIndex = 12;
+            this.soma.Text = "+";
+            this.soma.UseVisualStyleBackColor = true;
+            this.soma.Click += new System.EventHandler(this.soma_Click);
+            // 
+            // zero
+            // 
+            this.zero.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.zero.Location = new System.Drawing.Point(12, 334);
+            this.zero.Name = "zero";
+            this.zero.Size = new System.Drawing.Size(125, 51);
+            this.zero.TabIndex = 13;
+            this.zero.Text = "0";
+            this.zero.UseVisualStyleBackColor = true;
+            this.zero.Click += new System.EventHandler(this.zero_Click);
+            // 
+            // ponto
+            // 
+            this.ponto.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.ponto.Location = new System.Drawing.Point(154, 335);
+            this.ponto.Name = "ponto";
+            this.ponto.Size = new System.Drawing.Size(53, 51);
+            this.ponto.TabIndex = 14;
+            this.ponto.Text = ".";
+            this.ponto.UseVisualStyleBackColor = true;
+            this.ponto.Click += new System.EventHandler(this.ponto_Click);
+            // 
+            // subtracao
+            // 
+            this.subtracao.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.subtracao.Location = new System.Drawing.Point(225, 62);
+            this.subtracao.Name = "subtracao";
+            this.subtracao.Size = new System.Drawing.Size(53, 51);
+            this.subtracao.TabIndex = 15;
+            this.subtracao.Text = "-";
+            this.subtracao.UseVisualStyleBackColor = true;
+            this.subtracao.Click += new System.EventHandler(this.subtracao_Click);
+            // 
+            // divisao
+            // 
+            this.divisao.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.divisao.Location = new System.Drawing.Point(225, 131);
+            this.divisao.Name = "divisao";
+            this.divisao.Size = new System.Drawing.Size(53, 51);
+            this.divisao.TabIndex = 16;
+            this.divisao.Text = "/";
+            this.divisao.UseVisualStyleBackColor = true;
+            this.divisao.Click += new System.EventHandler(this.divisao_Click);
+            // 
+            // multiplicacao
+            // 
+            this.multiplicacao.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.multiplicacao.Location = new System.Drawing.Point(225, 200);
+            this.multiplicacao.Name = "multiplicacao";
+            this.multiplicacao.Size = new System.Drawing.Size(53, 51);
+            this.multiplicacao.TabIndex = 17;
+            this.multiplicacao.Text = "*";
+            this.multiplicacao.UseVisualStyleBackColor = true;
+            this.multiplicacao.Click += new System.EventHandler(this.multiplicacao_Click);
+            // 
+            // igual
+            // 
+            this.igual.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.igual.Location = new System.Drawing.Point(225, 269);
+            this.igual.Name = "igual";
+            this.igual.Size = new System.Drawing.Size(53, 117);
+            this.igual.TabIndex = 18;
+            this.igual.Text = "=";
+            this.igual.UseVisualStyleBackColor = true;
+            this.igual.Click += new System.EventHandler(this.igual_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(293, 401);
+            this.Controls.Add(this.igual);
+            this.Controls.Add(this.multiplicacao);
+            this.Controls.Add(this.divisao);
+            this.Controls.Add(this.subtracao);
+            this.Controls.Add(this.ponto);
+            this.Controls.Add(this.zero);
+            this.Controls.Add(this.soma);
+            this.Controls.Add(this.dois);
+            this.Controls.Add(this.tres);
+            this.Controls.Add(this.um);
+            this.Controls.Add(this.seis);
+            this.Controls.Add(this.sete);
+            this.Controls.Add(this.oito);
+            this.Controls.Add(this.nove);
+            this.Controls.Add(this.cinco);
+            this.Controls.Add(this.quatro);
+            this.Controls.Add(this.c);
+            this.Controls.Add(this.saida);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculadora - Gabriel Azevedo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox saida;
+        private System.Windows.Forms.Button c;
+        private System.Windows.Forms.Button quatro;
+        private System.Windows.Forms.Button cinco;
+        private System.Windows.Forms.Button nove;
+        private System.Windows.Forms.Button oito;
+        private System.Windows.Forms.Button sete;
+        private System.Windows.Forms.Button seis;
+        private System.Windows.Forms.Button um;
+        private System.Windows.Forms.Button tres;
+        private System.Windows.Forms.Button dois;
+        private System.Windows.Forms.Button soma;
+        private System.Windows.Forms.Button igual;
+        private System.Windows.Forms.Button multiplicacao;
+        private System.Windows.Forms.Button divisao;
+        private System.Windows.Forms.Button subtracao;
+        private System.Windows.Forms.Button ponto;
+        private System.Windows.Forms.Button zero;
+    }
+}
+
